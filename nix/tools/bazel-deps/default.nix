375f27fadecd3db64a822b23f1a0042205d9a35c
Andreas Herrmann <andreas.herrmann@tweag.io>
2019-04-16 17:37:30 +0200

    bazel-deps: Fix for Bazel 0.24.0

--- nix/tools/bazel-deps/default.nix
+++ nix/tools/bazel-deps/default.nix
@@ -74,7 +74,7 @@ buildBazelPackage rec {
       find . -type d -empty -delete
     '';
 
-    sha256 = "0xnwg847zkxf6vfmhsi1g16a4q78rgaa87prwcqhxl9psyl9r30c";
+    sha256 = "0kxb8xh9lqf2b3fx5ln3dm7dbs2min172xa6rdpvqmcl4vy73vcp";
   };
 
   buildAttrs = {
