d68af03e695e5a951211831f07e0ce2b72d114ba
Jonas Chevalier <42969603+jonas-chevalier-da@users.noreply.github.com>
2019-04-25 17:51:06 +0200

    ci: test the windows build without caching

--- ci/configure-bazel.sh
+++ ci/configure-bazel.sh
@@ -42,7 +42,7 @@ step "configuring bazel"
 
 if is_windows; then
   echo "build --config windows" > .bazelrc.local
-  echo "build --config windows-ci" >> .bazelrc.local
+  echo "build -s --verbose_failures" >> .bazelrc.local
 fi
 
 # sets up write access to the shared remote cache if the branch is not a fork
@@ -52,5 +52,5 @@ if [[ "${IS_FORK}" = False ]]; then
   echo "$GOOGLE_APPLICATION_CREDENTIALS_CONTENT" > "$GOOGLE_APPLICATION_CREDENTIALS"
   unset GOOGLE_APPLICATION_CREDENTIALS_CONTENT
   export GOOGLE_APPLICATION_CREDENTIALS
-  echo "build --remote_http_cache=https://storage.googleapis.com/daml-bazel-cache --remote_upload_local_results=true --google_credentials=${GOOGLE_APPLICATION_CREDENTIALS}" >> .bazelrc.local
+  #echo "build --remote_http_cache=https://storage.googleapis.com/daml-bazel-cache --remote_upload_local_results=true --google_credentials=${GOOGLE_APPLICATION_CREDENTIALS}" >> .bazelrc.local
 fi
